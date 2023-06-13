32f3cbbcc350e9863f0e6430c791afd011b2c3a9
vastavanand <golushailuoo7@gmail.com>
2023-06-13 10:21:55 +0530

    try-catch for generic email and whatsapp

--- process-10be.php
+++ process-10be.php
@@ -47,39 +47,51 @@ if (($handle = fopen($csv_file, "r")) !== FALSE)
     $csv_p = fopen($dir."/processed.csv", "w");
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE)
     {
-	$id_no = trim(str_replace( array(" ", ",", "-"), '', $data[1]));
-	$q = "select d_email, d_contact from dh_donor where d_id_no='".$id_no."' order by d_id desc limit 1";
-	//echo "$q\n";
-	$row = db_query($q)->fetchAssoc();
-        if ($row && $id_no)
-	{
-	   //echo "$id_no => ".$row['d_email']." -> ".$row['d_contact']."\n";
-	   $fields = array(  basename($data[0]), $id_no, $row['d_email'], $row['d_contact']);
-	   fputcsv($csv_p, $fields);
-	   $new_path = str_replace("unprocessed", "processed", $data[0]);
-	   $b_folder = dirname($new_path);
-     $tmp = explode("_", basename($data[0]));
+    	$id_no = trim(str_replace( array(" ", ",", "-"), '', $data[1]));
+    	$q = "select d_email, d_contact from dh_donor where d_id_no='".$id_no."' order by d_id desc limit 1";
+    	//echo "$q\n";
+    	$row = db_query($q)->fetchAssoc();
+      $email_sent = false;
+      $whatsapp_sent = false;
+      if ($row && $id_no && ( trim($row['d_email']) || trim($row['d_contact']) ))
+    	{
+        $row['d_email'] = trim($row['d_email']);
+        $row['d_contact'] = trim($row['d_contact']);
+    	   //echo "$id_no => ".$row['d_email']." -> ".$row['d_contact']."\n";
 
-	   if ($row['d_email'])
-	   {
-  		$body = "Dear ".$tmp[1]."\n\nPlease find attached Form 10 BE for financial year ".$fin."\n\nRegards,\n-Accounts Team\n($site)\n";
-  		send_email_generic($row['d_email'], $site." - Form 10 BE", nl2br($body), array($data[0]));
-	   }
-     if ($row['d_contact'])
-     {
-      send_whatsapp_generic($row['d_contact'], "form_10be_quick", array($tmp[1], $fin, $site), $data[0]);
-     }
+         $tmp = explode("_", basename($data[0]));
 
-	   if (! file_exists($b_folder))
-		    mkdir($b_folder, 0755, true);
+    	   if ($row['d_email'])
+    	   {
+      		$body = "Dear ".$tmp[1]."\n\nPlease find attached Form 10 BE for financial year ".$fin."\n\nRegards,\n-Accounts Team\n($site)\n";
+      		$ret_email = send_email_generic($row['d_email'], $site." - Form 10 BE", nl2br($body), array($data[0]));
+          $email_sent = $ret_email['success'];
+    	   }
+         if ($row['d_contact'])
+         {
+          $ret_whatsapp = send_whatsapp_generic($row['d_contact'], "form_10be_quick", array($tmp[1], $fin, $site), $data[0]);
+          $whatsapp_sent = $ret_whatsapp['success'];
+         }
+    	}
 
-	   rename($data[0], $new_path);
-	}
-	else
-	{
-	   $fields = array(  basename($data[0]), $id_no);
-	   fputcsv($csv_u, $fields);
-	}
+      if($email_sent || $whatsapp_sent)
+      {
+        $fields = array(  basename($data[0]), $id_no, $row['d_email'], $row['d_contact']);
+        fputcsv($csv_p, $fields);
+        $new_path = str_replace("unprocessed", "processed", $data[0]);
+        $b_folder = dirname($new_path);
+
+        if (! file_exists($b_folder))
+          mkdir($b_folder, 0755, true);
+
+        rename($data[0], $new_path);
+      }
+      else
+      {
+        $fields = array(  basename($data[0]), $id_no);
+        fputcsv($csv_u, $fields);
+      }
+
     }
     fclose($csv_p);
     fclose($csv_u);
